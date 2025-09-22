dc04df62b64d50f7efc580615f1fda02e49b4bf3
UmaanMehraj <umaanmehraj5@gmail.com>
2025-09-22 11:53:05 +0530

    fix: div size was not dynamic

---
+++ script.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('#container')
+
+const num = 32
+const square = num * num
+const x = 100 / num
+
+
+let count = 0
+
+
+while (count < square && (num >= 10 && num <= 100)) {
+    const div = document.createElement('div')
+    div.style.height = `${x}%`
+    div.style.width = `${x}%`
+    div.style.boxSizing = "border-box"
+    div.style.border = "1px solid silver"
+    container.appendChild(div)
+    count++;
+}
