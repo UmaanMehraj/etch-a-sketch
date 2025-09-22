6871e1e78c5cd79099b3a827438e0c3060cecea9
UmaanMehraj <umaanmehraj5@gmail.com>
2025-09-22 14:57:52 +0530

    feat: reset button

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
 const container = document.querySelector('#container')
 
-const num = 32
-const square = num * num
-const x = 100 / num
+let num = 16
+let square, numOfDivs
 
+const grid = document.querySelector('#grid')
+
+grid.addEventListener('click', () => {
+    num = parseInt(prompt('Enter number of divs (10 - 100): '))
+    if (num < 10 || num > 100) {
+        num = parseInt(prompt('Enter number of divs (10 - 100): '))
+    }
+
+    return num
+
+})
+
+square = num ** 2
+numOfDivs = 100 / num
+
+
+
+console.log(square)
+console.log(numOfDivs)
 
 let count = 0
 
 
 while (count < square && (num >= 10 && num <= 100)) {
     const div = document.createElement('div')
-    div.style.height = `${x}%`
-    div.style.width = `${x}%`
+    div.style.height = `${numOfDivs}%`
+    div.style.width = `${numOfDivs}%`
     div.style.boxSizing = "border-box"
     div.style.border = "1px solid silver"
     container.appendChild(div)
     count++
 }
 
-let child = document.querySelectorAll('#container > div')
+const child = document.querySelectorAll('#container > div')
 
 child.forEach(el => {
     el.addEventListener('mouseenter', () => {
         el.classList.add('div-black')
     })
+})
+
+const reset = document.querySelector('#reset')
+
+reset.addEventListener('click', () => {
+    child.forEach(el => {
+        el.classList.remove('div-black')
+    })
+
 })
 
